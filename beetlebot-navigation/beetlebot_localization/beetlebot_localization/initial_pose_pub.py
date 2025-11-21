3721b0ca368a155f7826d4ee62d7cad6fde1ac88
Ayisha318 <ayishaashraf318@gmail.com>
2025-11-21 10:37:40 +0200

    Convert beetlebot-navigation from submodule to standard package content.

---
+++ beetlebot-navigation/beetlebot_localization/beetlebot_localization/initial_pose_pub.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PointStamped
+
+class InitialPosePublisher(Node):
+
+    def __init__(self):
+        super().__init__('InitialPose_publisher')
+        self.publisher_ = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
+        self.subscription = self.create_subscription(PointStamped,'/clicked_point',self.clicked_point_callback,10)
+        self.subscription  # prevent unused variable warning
+        self.x = 0.0
+        self.y = 0.0
+
+    def clicked_point_callback(self, msg):
+        self.x = msg.point.x
+        self.y = msg.point.y
+        self.get_logger().info('X point: "%s"' % msg.point.x)
+        self.get_logger().info('Y point: "%s"' % msg.point.y)
+        self.initial_pose_publisher()
+
+    def initial_pose_publisher(self):
+        msg = PoseWithCovarianceStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "map"
+        msg.pose.covariance[0] = 0.25
+        msg.pose.covariance[7] = 0.25
+        msg.pose.covariance[35] = 0.06853891945200942
+        msg.pose.pose.position.x = self.x
+        msg.pose.pose.position.y = self.y
+        msg.pose.pose.position.z = 0.0
+        msg.pose.pose.orientation.z = 0.0
+        self.publisher_.publish(msg)
+        # self.get_logger().info('Publishing: "%s"' % msg.pose.pose.position.x)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    InitialPose_publisher = InitialPosePublisher()
+
+    rclpy.spin(InitialPose_publisher)
+
+    # Destroy the node explicitly
+    InitialPose_publisher.destroy_node()
+    rclpy.shutdown() 
+
+
+if __name__ == '__main__':
+    main()
---
+++ beetlebot-navigation/beetlebot_localization/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'beetlebot_localization'
+import os
+from glob import glob
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),     
+        (os.path.join('share', package_name, 'map'), glob('map/*')),    
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kronton',
+    maintainer_email='kyrlosfekry@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'initial_pose_pub = beetlebot_localization.initial_pose_pub:main',
+        ],
+    },
+)
